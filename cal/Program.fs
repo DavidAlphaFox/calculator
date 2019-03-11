8432b93089dba806a257760ce3104a4bfeba6e33
David.Gao <david.alpha.fox@gmail.com>
2019-03-11 23:28:10 +0800

    Reverse Polish Notation

---
+++ cal/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Calculator.RpnCalculator
+
+[<EntryPoint>]
+let main argv =
+    [ "4 2 5 * + 1 3 2 * + /"; "5 4 6 + /"; "10 4 3 + 2 * -"; "2 3 +";
+    "90 34 12 33 55 66 + * - + -"; "90 3 -" ]
+    |> List.map (fun expr -> expr, evalRpnExpr(expr.Split(' ')))
+    |> List.iter (fun (expr, result) -> printfn "(%s) = %A" expr result)
+    Console.ReadLine() |> ignore
+    0
---
+++ cal/RpnCalculator.fs
@@ -0,0 +1,12 @@
+﻿module Calculator.RpnCalculator
+
+let evalRpnExpr (stack : string[]) =
+    let solve items current =
+        match (current, items) with
+        | "+", y::x::t -> (x + y)::t
+        | "-", y::x::t -> (x - y)::t
+        | "*", y::x::t -> (x * y)::t
+        | "/", y::x::t -> (x / y)::t
+        | _ -> (float current)::items
+    let result = Seq.fold solve [] stack
+    result.Head
